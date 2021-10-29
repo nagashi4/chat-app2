de24babbdd3d89ae2b272654b152342824043f6f
nagashi4 <92012577+nagashi4@users.noreply.github.com>
2021-10-29 13:51:11 +0900

    メッセージ送信機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   
-  has_many :room_users
-  has_many :rooms, through: :room_users
-  has_many :messages
-
-  validates :name, presence: true
-
-end
+         has_many :room_users
+         has_many :rooms, through: :room_users
+         has_many :messages
+       
+         validates :name, presence: true
+       end
